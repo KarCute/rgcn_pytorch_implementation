cf1adeca59f395a1c5f0973ed2a83ea57efb1199
KarCute <792184748@qq.com>
2019-08-11 18:14:50 +0800

    update .pkl and add load_data(generate .pkl file)

---
+++ data_conversion.py
@@ -0,0 +1,128 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+
+def encode_onehot(labels):
+    classes = set()
+    for label in labels:
+        classes |= set(label)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array([np.sum([classes_dict.get(l) for l in label], axis=0) for label in labels], dtype=np.int32)
+    return labels_onehot, len(classes)
+
+# load dataset for rgcn
+# {adjacencies, features, labels, labeled_nodes_idx, train_idx, test_idx, relations_dict, train_names, test_names}
+def load_data_fb (path, dataset):
+    print('Loading {} dataset...'.format(dataset))
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+
+    # Get features
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    features = sp.csr_matrix(features)
+    print('features: ', features)
+
+    # Get labels and labeled_nodes_idx
+    labels = list(map(lambda x: x.split(','), idx_features_labels[:, -1]))
+    labels, nclass = encode_onehot(labels)
+    labels = sp.csr_matrix(labels)
+    labeled_nodes_idx = list(labels.nonzero()[0])
+    print('labels: ', labels)
+
+    # Get relations_dict(temporarily unknown)
+    rel_dict = {}
+    idx_rel = np.genfromtxt("{}{}.rel".format(path, dataset), dtype=np.dtype(str))
+    for index in range(len(idx_rel)):
+        e1 = idx_rel[: , 0]
+        rel_dict[e1[index]] = index
+    print('rel_dict: ', rel_dict)
+
+    # my method get classes adjancencies
+    value = []
+    row = []
+    col = []
+    for i in range(len(idx_rel)):
+        value.append([])
+        row.append([])
+        col.append([])
+    adjacencies = []
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
+    # 将每组关系中的entity用索引表示
+    edges = np.array(list(map(idx_map.get, edges_unordered[:, :2].flatten())), dtype=np.int32).reshape(edges_unordered[:, :2].shape)
+    e1 = np.array(edges[:, 0],dtype=np.int32)
+    e2 = np.array(edges[:, 1],dtype=np.int32)
+    relation = np.array(edges_unordered[:, -1],dtype=np.int32)
+    amount = len(relation)
+    for index in range(amount):
+        value[relation[index]].append(1)
+        row[relation[index]].append(e1[index])
+        col[relation[index]].append(e2[index])
+    for index in range(len(idx_rel)):
+        adjacencies.append(sp.coo_matrix((value[index],(row[index],col[index])), shape=(labels.shape[0],labels.shape[0]), dtype=np.float32))
+    print("adj1: {}".format(adjacencies[1]))
+    print("adj2: {}".format(adjacencies[2]))
+    print("adj3: {}".format(adjacencies[3]))
+
+    # Divide train_idx and test_idx
+    train_idx = range(len(idx_map) // 10 * 8)
+    val_idx = range(len(idx_map) // 10 * 8, len(idx_map) // 10 * 9)
+    test_idx = range(len(idx_map) // 10 * 9, len(idx_map))
+    name = np.array(idx_features_labels[:, 0],dtype=str)
+    train_name = name[0 : len(name) // 10 * 8]
+    val_name = name[len(name) // 10 * 8 : len(name) // 10 * 9]
+    test_name = name[len(name) // 10 * 9 : len(name)]
+    print('train_idx: ',train_idx)
+    print('test_idx: ', test_idx)
+    print('train_name: ', train_name)
+    print('test_name: ', test_name)
+
+
+    print('Loading {} dataset finishes...'.format(dataset))
+    return adjacencies,features,labels,labeled_nodes_idx,train_idx,test_idx,val_idx,rel_dict,train_name,test_name,val_name
+
+def load_data_cora(path,dataset):
+    print('Loading {} dataset...'.format(dataset))
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+
+    # Get features
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    features = sp.csr_matrix(features)
+    print('features: ', features)
+
+    # Get labels and labeled_nodes_idx
+    labels = list(map(lambda x: x.split(','), idx_features_labels[:, -1]))
+    labels, nclass = encode_onehot(labels)
+    labels = sp.csr_matrix(labels)
+    labeled_nodes_idx = list(labels.nonzero()[0])
+    print('labels: ', labels)
+
+    # Get relations_dict(temporarily unknown)
+    rel_dict = {'0':0}
+
+    # my method get classes adjancencies
+    adjacencies = []
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
+    # 将每组关系中的entity用索引表示
+    edges = np.array(list(map(idx_map.get, edges_unordered[:, :2].flatten())), dtype=np.int32).reshape(edges_unordered[:, :2].shape)
+    # 构建图的邻接矩阵，用坐标形式的稀疏矩阵表示，非对称邻接矩阵
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+    adjacencies.append(adj)
+    print('adj: ', adjacencies[0])
+
+
+    # Divide train_idx and test_idx
+    train_idx = range(len(idx_map) // 10 * 8)
+    test_idx = range(len(idx_map) // 10 * 8, len(idx_map))
+    name = np.array(idx_features_labels[:, 0], dtype=str)
+    train_name = name[0: len(name) // 10 * 8]
+    test_name = name[len(name) // 10 * 8: len(name)]
+    print('train_idx: ', train_idx)
+    print('test_idx: ', test_idx)
+    print('train_name: ', train_name)
+    print('test_name: ', test_name)
+
+    print('Loading {} dataset finishes...'.format(dataset))
+    return adjacencies, features, labels, labeled_nodes_idx, train_idx, test_idx, rel_dict, train_name, test_name
---
+++ prepare_dataset.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+
+from data_utils import load_data
+from data_conversion import *
+from utils import *
+
+import pickle as pkl
+
+import os
+import sys
+import time
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", type=str, default="aifb",
+                help="Dataset string ('aifb', 'mutag', 'bgs', 'am')")
+
+args = vars(ap.parse_args())
+
+print(args)
+
+# Define parameters
+DATASET = args['dataset']
+
+NUM_GC_LAYERS = 2  # Number of graph convolutional layers
+
+# Get data
+if DATASET == 'aifb' or DATASET == 'am' or DATASET == 'bgs' or DATASET == 'mutag':
+    A, X, y, labeled_nodes_idx, train_idx, test_idx, rel_dict, train_names, test_names = load_data(
+    DATASET)
+elif DATASET == 'cora':
+    A, X, y, labeled_nodes_idx, train_idx, test_idx, val_idx, rel_dict, train_names, test_names, val_names = load_data_cora('./data/'+ DATASET + '/',
+    DATASET)
+else:
+    A, X, y, labeled_nodes_idx, train_idx, test_idx, val_idx, rel_dict, train_names, test_names, val_names = load_data_fb('./data/'+ DATASET + '/',
+    DATASET)
+
+# rel_list = list(range(len(A)))
+# for key, value in rel_dict.items():
+#     if value * 2 >= len(A):
+#         continue
+#     rel_list[value * 2] = key
+#     rel_list[value * 2 + 1] = key + '_INV'
+
+
+num_nodes = A[0].shape[0] # 8285
+A.append(sp.identity(A[0].shape[0]).tocsr())  # add identity matrix
+
+support = len(A) # 91
+
+print("Relations used and their frequencies" + str([a.sum() for a in A]))
+
+print("Calculating level sets...")
+t = time.time()
+# Get level sets (used for memory optimization)
+bfs_generator = bfs_relational(A, labeled_nodes_idx)
+lvls = list()
+lvls.append(set(labeled_nodes_idx))
+lvls.append(set.union(*bfs_generator.__next__()))
+print("Done! Elapsed time " + str(time.time() - t))
+
+# Delete unnecessary rows in adjacencies for memory efficiency
+todel = list(set(range(num_nodes)) - set.union(lvls[0], lvls[1]))
+for i in range(len(A)):
+    csr_zero_rows(A[i].tocsr(), todel)
+
+data = {'A': A,
+        'y': y,
+        'train_idx': train_idx,
+        'test_idx': test_idx,
+        'val_idx':val_idx
+        }
+
+dirname = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+with open(dirname + '/' + DATASET + '.pickle', 'wb') as f:
+    pkl.dump(data, f, pkl.HIGHEST_PROTOCOL)
